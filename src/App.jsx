bfa522234ef8b8368a22c35b45a19c50f64c2bf2
Nishant Jadhav <nishant@guyal.com>
2025-11-25 22:32:15 +0530

    Contrive Designs Dev

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import "./App.css";
 import Highlights from "./components/Highlights";
 import Footer from "./components/Footer";
 import Testimonials from "./components/Testimonials";
-import Navbar_m from "./components/Navbar_m";
-import Hero_m from "./components/Hero_m";
-import Footer_m from "./components/Footer_m";
 
 function App() {
   return (
